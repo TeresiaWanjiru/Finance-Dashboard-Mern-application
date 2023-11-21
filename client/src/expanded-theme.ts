ea9b300688f5691bebf83054ec6f359a14880d59
TeresiaWanjiru <144387301+TeresiaWanjiru@users.noreply.github.com>
2023-11-21 14:55:28 +0300

    eslint import rules

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,6 +1,5 @@
 //in this file we are extending the alreading existing themes that MUI has provided typescript
-
-//eslint-disable-next-line
+// eslint-disable-next-line
 import { Palette,PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
